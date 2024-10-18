2a057d477d6a72c326f3e79185432ebc7327ea5c
affq <adrianfabisiewicz@gmail.com>
2024-10-18 10:37:20 +0200

    wetransfer

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import numpy
-
+from email.message import EmailMessage
 from osgeo import gdal
 gdal.UseExceptions()
-
 from typing import Union
 from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+import geopandas as gpd
+
 def read_spatial_raster(path: Union[str, Path]) -> gdal.Dataset:
     dataset = gdal.Open(str(path))
     assert dataset is not None, "Read spatial raster returned None"
@@ -12,13 +14,23 @@ def read_spatial_raster(path: Union[str, Path]) -> gdal.Dataset:
 
 raster_file = "N-34-A-a-1-1.tif"
 raster_dataset = read_spatial_raster(raster_file)    
-print("Natywny układ współrzędnych rastra:", 
-raster_dataset.GetProjection())
-print("Patametry transformacji z układu XY do układu UV rastra:", 
-raster_dataset.GetGeoTransform())
-print("ok")
-exit()
 
-print("Liczba kanałów:", raster_dataset.RasterCount)
-print("Wymiary rastra w pikselach (szerokość x wysokość):", 
-[raster_dataset.RasterXSize, raster_dataset.RasterYSize])
+band = raster_dataset.GetRasterBand(1)
+array = band.ReadAsArray()
+array = np.copy(array)
+
+vector_features = gpd.read_file(R"budynki1\budynki.shp")
+example_point = vector_features.iloc[0]['geometry'].centroid.xy
+example_point = np.float64(example_point)
+
+def point_to_pixel(x, y, geotransform):
+    c, a, b, f, d, e = geotransform
+    column = (x - c) / a
+    row = (y - f) / e
+    return row, column  # ij convention to stay with NumPy
+
+i, j = point_to_pixel(example_point[0], example_point[1], raster_dataset.GetGeoTransform())
+
+plt.imshow(array, cmap='grey')
+plt.scatter(j, i)
+plt.show()
